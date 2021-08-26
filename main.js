e6729f97d786c1b11d3e76751ed5dfa1ae7b76f4
Jonjett Moralita <88562394+Jonjett@users.noreply.github.com>
2021-08-26 13:06:42 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.urlencoded({extended:false}))
+
+
+app.get("/" , (req, res) => {
+    res.send(`
+        <div>
+            <h1> Welcome to my first Web Server...</h1>
+        </div>
+        <form action='/FillUp' method=GET>
+            <button>FILL UP FORM</button>
+        </form>
+    `)
+})
+
+app.get("/FillUp", (req, res) => {
+    res.render('index')
+})
+
+app.post('/hrform1', (req, res) => {
+    console.log(req.body)
+    res.send(`
+    <div>
+        <h2> Thanks for your submission...</h2>
+    </div>
+    <form action='/' method=GET>
+        <button>HOME</button>
+    </form>
+    <form action='/FillUp' method=GET>
+        <button>Fill Up Again</button>
+    </form>
+    `)
+})
+
+app.listen(process.env.PORT || 3000) 
