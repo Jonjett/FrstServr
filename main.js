74199c567cff1d4ac950ced2a6e57124bbf8a398
Jonjett Moralita <88562394+Jonjett@users.noreply.github.com>
2021-09-18 06:29:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const cors = require('cors')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
-app.use(cors())
+
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
 
